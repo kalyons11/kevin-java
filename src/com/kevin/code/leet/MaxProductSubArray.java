ac1eb3c7a7715d15cf0710ba41129fdd2b48eb77
kalyons11 <kevinandrewlyons@gmail.com>
2019-12-18 19:54:01 -0500

    Add initial scaffold for leet problem

---
+++ src/com/kevin/code/leet/MaxProductSubArray.java
@@ -0,0 +1,18 @@
+package com.kevin.code.leet;
+
+public class MaxProductSubArray {
+    
+    /*
+     * Given an integer array nums, find the contiguous subarray
+     * within an array (containing at least one number) which has
+     * the largest product.
+     */
+    public int maxProduct(final int[] nums) {
+        // Formulate as DP problem
+        // DP(i, j) = max product of contiguous subarray from i (inc)
+        // to j (exc)
+        // TODO implement
+        return -1;
+    }
+
+}
